b4400fe5309eb86ba9e9b917195e2fad98753310
Bruno Abreu <88842907+brabreus@users.noreply.github.com>
2023-09-19 10:56:49 -0300

    Update SEG7_LUT.v

--- SEG7_LUT.v
+++ SEG7_LUT.v
@@ -1,11 +1,12 @@
-module SEG7_LUT	(	oSEG,iDIG	);
-input	[3:0]	iDIG;
-output	[6:0]	oSEG;
-reg		[6:0]	oSEG;
+module SEG7_LUT	(
+	input wire [3:0] iDIG,
+	output reg [6:0] oSEG
+);
 
 always @(iDIG)
 begin
-		case(iDIG)
+	case(iDIG)
+		4'h0: oSEG = 7'b1000000;
 		4'h1: oSEG = 7'b1111001;	// ---t----
 		4'h2: oSEG = 7'b0100100; 	// |	  |
 		4'h3: oSEG = 7'b0110000; 	// lt	 rt
@@ -21,8 +22,7 @@ begin
 		4'hd: oSEG = 7'b0100001;
 		4'he: oSEG = 7'b0000110;
 		4'hf: oSEG = 7'b0001110;
-		4'h0: oSEG = 7'b1000000;
-		endcase
+	endcase
 end
 
 endmodule
