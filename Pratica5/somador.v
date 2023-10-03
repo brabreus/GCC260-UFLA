3045e9b852afb5af167e8a5239831bec36c5114f
Bruno Abreu <88842907+brabreus@users.noreply.github.com>
2023-10-03 10:26:33 -0300

    Update somador.v

--- Pratica5/somador.v
+++ Pratica5/somador.v
@@ -6,14 +6,12 @@ module somador
 		input wire cin,
 		input wire [N-1:0] A, B,
 		output reg [N-1:0] S,
-		output reg cout,
-		output reg overflow
+		output reg cout
 	);
 	
 	always@*
 	begin
 		{cout,S} = A + B + cin;
-		overflow = (A[N-1] & B[N-1] & ~S[N-1]) | (~A[N-1] & ~B[N-1] & S[N-1]);
 	end
 	
 	
